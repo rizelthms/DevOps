6410c591eb3e34164c48472fed06a980753fb86b
Rizelthomas <497836@student.saxion.nl>
2023-03-02 15:20:17 +0100

    Added comments to existing trash.sh

--- week2/assignment2-advanced/trash.sh
+++ week2/assignment2-advanced/trash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Define the function to print usage instructions
 function printUsage() {
     echo "Usage:
     trash <file>     : Moves the file to the trash.
@@ -7,20 +8,22 @@ function printUsage() {
     trash -r <file>  : Moves the file from the trash to the current directory."
 }
 
+#Get the current user's home directory
 user_home=$(bash -c "cd ~$(printf %q "$USER") && pwd")
 
+# Parse command line arguments using getopts
 while getopts ":lr:" opt; do
     case $opt in
-    l)
+    l) 
         echo "files in the trash:"
-        ls -al ~/trash | grep -v '^d'
+        ls -al ~/trash | grep -v '^d' # exclude directories from the list
         ;;
     r)
         FILE=$OPTARG
-        if [ ! -f ~/trash/"${FILE}" ]; then
+        if [ ! -f ~/trash/"${FILE}" ]; then # check if the file exists in trash
             echo "not a file"
         else
-            mv ~/trash/"${FILE}" .
+            mv ~/trash/"${FILE}" . # move file from trash to current directory
         fi
         ;;
     \?)
@@ -36,23 +39,25 @@ while getopts ":lr:" opt; do
     esac
 done
 
+# If no options were specified, assume the first argument is a file to be moved to trash
 if [ $OPTIND -eq 1 ]; then
     if [ $# -eq 1 ]; then
 
         FILE=$1
 
+    # Create the trash directory if it doesn't exist yet
         if [ ! -d "$user_home"/trash ]; then
             mkdir "$user_home"/trash
             echo "trash directory created"
         fi
 
-        if [ ! -f "$FILE" ]; then
+        if [ ! -f "$FILE" ]; then # check if file exists
             echo "not a file"
         else
-            mv "$FILE" ~/trash
+            mv "$FILE" ~/trash # move file to trash directory
         fi
 
-    else
+    else # If there are no options and more than one argument, print usage instructions
         printUsage
         exit 1
     fi
