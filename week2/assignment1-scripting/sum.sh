18c194cbecac818f0a0ded0b0b3ca095f12b6bd1
Daniel Bya <peachtuber@gmail.com>
2023-03-06 09:29:54 +0100

    1.5 Numbers

--- week2/assignment1-scripting/sum.sh
+++ week2/assignment1-scripting/sum.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+
 if [ $# == 0 ]
 then
     echo "Please supply a data set"
 else
-    declare -i SUM=0
-    while read -r X ; do
-        SUM+=$X
-    done < "$1"
+    SUM=$(awk '{ total += $1 } END { print total }' "$1")
     echo "Sum is: $SUM"
 fi
+
 
