a0566c202d206de814c2603ac0b668512599dc3f
Rizelthomas <497836@student.saxion.nl>
2023-03-21 10:14:47 +0100

    7. Bring it all together
    run a frontend and backend application in separate containers

--- week4/todo.sh
+++ week4/todo.sh
@@ -9,13 +9,22 @@ elif [[ $1 -eq 2 ]]; then
 elif [[ $1 -eq 3 ]]; then
     docker run --name reverseProxy -p 8080:80 -v 2-reverse-proxy:/etc/nginx/conf.d nginx
 elif [[ $1 -eq 4 ]]; then
-    echo todo4
-# TODO: ./todo.sh 4
-#       Clone and run the simple-todo-app using an nginx container.
-#       Clone the simple-todo-app from https://gitlab.com/sealy/simple-todo-app
-#       Navigate to the project folder and create a build distribution (using npm run build)
-#       Run an nginx container that has a volume mapping to the build distribution (the dist folder)
+cd 4-complete/frontend
+git clone https://gitlab.com/sealy/simple-todo-app.git
+cd simple-todo-app
 
+git checkout backend-connection
+
+npm install
+npm run build
+
+cd ../../
+
+cd backend
+git clone https://gitlab.com/sealy/simple-todo-backend.git
+
+cd ..
+docker compose up
 
 elif [[ $1 -eq 5 ]]; then
 # TODO: ./todo.sh 5
