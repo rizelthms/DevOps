e7996b5ece357430ca926635ee6fd1df9a687ef8
Rizelthomas <497836@student.saxion.nl>
2023-04-04 11:19:07 +0200

    Creating a VPC with one public subnet

--- week6/main.tf
+++ week6/main.tf
@@ -1,1 +1,21 @@
-# Your code here
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+    tags = {
+    Name = "MainVpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+    tags = {
+    Name = "Subnet1"
+  }
+}
