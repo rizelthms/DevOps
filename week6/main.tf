da1a03c0d6b21b04c29392371c36cc9341a39d73
Rizelthomas <497836@student.saxion.nl>
2023-04-04 12:05:57 +0200

    setting up the routing

--- week6/main.tf
+++ week6/main.tf
@@ -19,3 +19,28 @@ resource "aws_subnet" "my_subnet" {
     Name = "Subnet1"
   }
 }
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "MainIGW"
+  }
+}
+
+resource "aws_route_table" "main_route_table" {
+vpc_id = aws_vpc.main_vpc.id
+
+route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+}
+  tags = {
+    Name = "MainRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "main_route_table_association" {
+    subnet_id = aws_subnet.my_subnet.id
+    route_table_id = aws_route_table.main_route_table.id
+}
