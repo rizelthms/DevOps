c63227cc7cfdb67d575d6a0de68b37d9c96b2ccd
Daniel Bya <peachtuber@gmail.com>
2023-04-10 23:18:13 +0200

    week 6 ex 5 second public subnet

--- week6/main.tf
+++ week6/main.tf
@@ -2,21 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "subnets" {
+  type    = set(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 
-    tags = {
+  tags = {
     Name = "MainVpc"
   }
 }
 
 resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  for_each                = toset(var.subnets)
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = each.value
   map_public_ip_on_launch = true
 
-    tags = {
-    Name = "Subnet1"
+  tags = {
+    Name = "Subnet${each.key}"
   }
 }
 
@@ -29,58 +35,72 @@ resource "aws_internet_gateway" "main_igw" {
 }
 
 resource "aws_route_table" "main_route_table" {
-vpc_id = aws_vpc.main_vpc.id
+  vpc_id = aws_vpc.main_vpc.id
 
-route{
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main_igw.id
-}
+  }
   tags = {
     Name = "MainRouteTable"
   }
 }
 
 resource "aws_route_table_association" "main_route_table_association" {
-    subnet_id = aws_subnet.my_subnet.id
-    route_table_id = aws_route_table.main_route_table.id
+  for_each       = aws_subnet.my_subnet
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.main_route_table.id
 }
 
+# resource "aws_instance" "main_instance" {
+#   ami             = "ami-007855ac798b5175e"
+#   instance_type   = "t2.micro"
+#   subnet_id       = aws_subnet.my_subnet.id
+#   key_name        = "MainKey"
+#   security_groups = [aws_security_group.main_security_group.id]
+
+#   user_data = templatefile("${path.module}/templates/nginx_config.sh", {})
+
+#   tags = {
+#     Name = "MainInstance"
+#   }
+# }
 resource "aws_instance" "main_instance" {
-  ami = "ami-007855ac798b5175e"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.my_subnet.id
-  key_name      = "MainKey"
+  ami             = "ami-007855ac798b5175e"
+  instance_type   = "t2.micro"
+  subnet_id       = values(aws_subnet.my_subnet)[0].id
+  key_name        = "MainKey"
   security_groups = [aws_security_group.main_security_group.id]
 
   user_data = templatefile("${path.module}/templates/nginx_config.sh", {})
 
-      tags = {
+  tags = {
     Name = "MainInstance"
   }
 }
 
 resource "aws_security_group" "main_security_group" {
-  name = "MainSG"
+  name   = "MainSG"
   vpc_id = aws_vpc.main_vpc.id
-  
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    }
+  }
 
-   egress {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-   }
+  }
 }
